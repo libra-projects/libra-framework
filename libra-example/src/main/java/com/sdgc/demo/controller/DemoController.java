cb55816599a5a7ad754abcf4da25c1f0a3f180b5
titeng.jiang <titeng.jiang@ele.me>
2017-09-08 23:38:47 +0800

    intergrate with mybatis && spring-mybatis-start

--- libra-example/src/main/java/com/sdgc/demo/controller/DemoController.java
+++ libra-example/src/main/java/com/sdgc/demo/controller/DemoController.java
@@ -1,8 +1,12 @@
 package com.sdgc.demo.controller;
 
+import com.sdgc.demo.mapper.DemoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by titengjiang on 2017/9/7.
@@ -10,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+
+    @Autowired
+    private DemoMapper mapper;
+
     @RequestMapping(method = RequestMethod.GET)
     public String demo() {
+        List<Object> results = mapper.getAll();
         return "helloLibra";
     }
 }
